e0b9d42f17943bc4e18ec7b21d2d26492b75bd92
StylePoints420 <stylepoints420@gmail.com>
2023-10-26 22:10:02 +0800

    Add condition and loop to repeat strings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(let i = 0;i <= num; i++) {
+        return string.repeat(num);
+        // console.log(string);
+    }
 };
 
 // Do not edit below this line
