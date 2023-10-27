227b608a07e21965ab84f46bebddc7c826f21a0b
StylePoints420 <stylepoints420@gmail.com>
2023-10-27 15:54:50 +0800

    Added for loop to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let text = "";
+    for (let i = word.length;i > 0; i--) {
+        text += word[i-1];
+    }
+    return text;
 };
 
 // Do not edit below this line
