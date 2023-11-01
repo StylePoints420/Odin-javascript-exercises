75cd5cdee072ae158f0c17e4a540735828521b06
StylePoints420 <stylepoints420@gmail.com>
2023-11-01 23:40:28 +0800

    Added function for sum of 2 numbers between them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNum, lastNum) {
+    let errorMessage = "ERROR";
+    let totalSum = 0;
+    if (startNum > lastNum) {
+        for(let i = lastNum; i <= startNum; i++) {
+        totalSum += i;
+        }
+        return totalSum;
+    }
 
+    else if (startNum < 0 || lastNum < 0) {
+        return errorMessage;
+    }
+
+    else if (typeof startNum !== "number" || typeof lastNum !== "number") {
+        return errorMessage;
+    }
+
+    else {
+        for(let i = startNum; i <= lastNum; i++) {
+        totalSum += i;
+        }
+        return totalSum;
+    }
 };
 
 // Do not edit below this line
