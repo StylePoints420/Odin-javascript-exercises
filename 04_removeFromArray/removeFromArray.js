469b406e96a4aec91358c40cc4a9e4afd059fad1
StylePoints420 <stylepoints420@gmail.com>
2023-11-01 22:30:58 +0800

    Added function to remove specific array
    PS. AAAAAAAAAA IT WAS SO HARD, I WAS STUCK FOR A LONG TIME

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...valuesToRemove) {
+    for(let i of valuesToRemove) {
+        if(arr.includes(i) == true) {
+            arr.splice(arr.indexOf(i), 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
